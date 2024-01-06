2f58d5ef46a444b17ae19783a93659266c110b3f
Numaan <mirnumaan06@gmail.com>
2024-01-06 01:11:55 +0000

    new File

---
+++ index.js
@@ -0,0 +1,63 @@
+// var gender = "male";
+// var LName = "Ahmed";
+// if (gender == "f" || gender == "female") {
+//   console.log("hello", "how are you?" + LName);
+// } else {
+//   console.log("only for girls");
+// }
+// var answer = 0;
+// for (i = 0; i <= 100; i++) {
+//   answer = answer + i;
+
+// }
+
+// console.log(answer);
+
+// Array
+
+// const ar = [0, 1, 2, 4, 5, 6, 7, 7, 8, 9];
+
+// for (i = 0; i < ar.length; i++) {
+//   console.log(ar[i]);
+//   if (ar[i] % 2 == 0) {
+//     console.log(ar[i], "is an even number");
+//   }
+// }
+
+// const a = [{
+//   a: "Rakesh",
+//   gender: "male",
+// },{
+//   a: "reshma",
+//   gender: "female",}
+// ];
+
+// for (i in a) {
+// if(a[i]['gender']=='male'){
+//   console.log(a[i]['a']);
+// }
+
+// }
+
+// function findSum(a, b, addSum) {
+//   const sum1 = addSum(a,b);
+//   return sum1;
+// }
+
+// function sum(a,b) {
+//   return a + b;
+// }
+
+// var a = add1Sum(2,4,findSum  );
+// console.log(a);
+
+var a ="hello";
+console.log(a)
+
+function ifrah(a){
+  
+  return "my name is " +a
+}
+
+const name = ifrah("ifrah");
+console.log(name);
